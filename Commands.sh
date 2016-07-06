5409339e572d602a084c12f0f3dceaf38c1b5f4c
Adam Pavlidis <adampavlidis@gmail.com>
2016-07-06 13:01:24 +0300

    Added command to upload ssh keys

--- Commands.sh
+++ Commands.sh
@@ -1,2 +1,5 @@
 # find a pattern in all files contained in a specific filesystem tree
 find . -type f -print | sed 's/ /\\ /g' | xargs grep -i "my text string"
+
+# Appends the public key to a server through ssh
+ssh user@server "echo \"`cat .ssh/id_rsa.pub`\" >> .ssh/authorized_keys"
