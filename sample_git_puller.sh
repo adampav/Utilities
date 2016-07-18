c12eb0568a31e5184f48e38d69ef74253b020426
Adam Pavlidis <adampavlidis@gmail.com>
2016-07-18 14:38:19 +0300

    sample bash script to pull from remote git repo

---
+++ sample_git_puller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Attention! Restricted Area!!!"
+echo "Just about to pull latest from origin"
+sleep 1
+
+read -n1 -p "Do you to continue, and assume responsibility? [y,n] `echo $'\n>'`" outp
+echo -e ""
+case $outp in
+  y|Y) date; git pull ;;
+  n|N) echo -e "Please go on Captain!" ;;
+  *)   echo -e "Wrong input, pulling for good measure"; date; git pull ;;
+esac
